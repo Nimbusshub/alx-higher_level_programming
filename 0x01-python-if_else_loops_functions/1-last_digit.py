ec453a2ce9030fb4156b06680fe52a0287a108f7
Nimah <busayonimota1603@gmail.com>
2022-05-31 18:28:36 +0100

    [Fix] Error: wrong type output corrected

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastD = int(repr(number)[-1])
 if number < 0:
-    lastD *= -1
+    lastD = number % -10
+else:
+    lastD = number % 10
 print("Last digit of {} is {}".format(number, lastD), end=" ")
 if lastD < 6 and lastD != 0:
     print("and is less than 6 and not 0")
