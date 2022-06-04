7efebc62b57b827ef6621924a96bd59fb1e88f77
Nimah <busayonimota1603@gmail.com>
2022-06-04 20:28:14 +0100

    [Feature] print all int in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx >= len(my_list) or idx < 0:
-        return my_list
-    b = list(my_list)
-    for i in range(len(b)):
-        if i == idx:
-            b[i] = element
-    return b
--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    my_string = my_string.translate({ord('c'): ''})
-    my_string = my_string.translate({ord('C'): ''})
-    return my_string
