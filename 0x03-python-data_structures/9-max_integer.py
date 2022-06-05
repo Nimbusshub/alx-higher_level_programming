10db9cb7097f19cf00a3590c6349a37f06520df5
Nimah <busayonimota1603@gmail.com>
2022-06-05 19:02:32 +0100

    Return the max of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    for i in range(len(my_list)):
+        if my_list[0] < my_list[i]:
+            my_list[0] = my_list[i]
+    return my_list[0]
