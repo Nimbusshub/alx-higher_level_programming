c639e3bbed4ca96070f9bd3ec9965ce278c94f24
Nimah <busayonimota1603@gmail.com>
2022-06-23 17:10:33 +0100

    import removed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from xml.dom import SyntaxErr
-
 
 def safe_print_list(my_list=[], x=0):
     j = 0
@@ -9,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             if j < x:
                 print("{}".format(i), end="")
                 j += 1
-    except SyntaxErr:
+    except SyntaxError:
         print("{:s}".format("Error"))
     print()
     return j
