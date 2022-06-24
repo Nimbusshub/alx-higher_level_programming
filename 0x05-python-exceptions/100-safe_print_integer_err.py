b2166cac91ce67df30da38b9eaec84753c9b40d7
Nimah <busayonimota1603@gmail.com>
2022-06-24 11:43:10 +0100

    remove trailing whitespace after exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except TypeError as te:
-        print("Exception: ", te, file=stderr)
+        print("Exception:", te, file=stderr)
         return False
     except ValueError as ve:
-        print("Exception: ", ve, file=stderr)
+        print("Exception:", ve, file=stderr)
         return False
