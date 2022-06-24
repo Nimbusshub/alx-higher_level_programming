ceb13e324124fc71c539eeed31bec34e45258cfd
Nimah <busayonimota1603@gmail.com>
2022-06-24 10:04:18 +0100

    print division and handle exception

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        c = round(a/b, 1)
+    except ZeroDivisionError:
+        c = None
+    finally:
+        print("{}: {}".format("Inside Result", c))
+    return c
