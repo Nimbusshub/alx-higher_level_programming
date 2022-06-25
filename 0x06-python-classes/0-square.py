61ae90a9b6d7bb52ed670501d672a381fe53d33f
Nimah <busayonimota1603@gmail.com>
2022-06-25 18:19:53 +0100

    define an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A simple defination of a class.
+An empty class Square that defines a square """
+
+
+class Square:
+    """A class with no attribute or method"""
+    pass
+
+
+help(Square)
