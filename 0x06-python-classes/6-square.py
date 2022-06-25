109380a7e9c5c056d29792597175f2515f96f5a3
Nimah <busayonimota1603@gmail.com>
2022-06-26 00:53:07 +0100

    add private instance attribute position to the class Square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -64,11 +64,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) and type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] and value[1] < 0:
+        if (type(value) is not tuple or
+            type(value[0]) and type(value[1]) is not int or
+                value[0] and value[1] < 0 or
+                len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
