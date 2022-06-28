e36b411b4b5598eaee2a2495bec1cfac394a5664
Nimah <busayonimota1603@gmail.com>
2022-06-28 02:23:00 +0100

    Resolve the N-queen puzzle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -103,20 +103,12 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        # for i in range(self.__height):
-        #     if i != self.__height and i != 0:
-        #         print()
-        #     print(str(self.print_symbol) * self.__width, end="")
+        for i in range(self.__height):
+            if i != self.__height and i != 0:
+                print()
+            print(str(self.print_symbol) * self.__width, end="")
 
-        # return ""
-        rect = []
-        for x in range(self.__height):
-            for y in range(self.__width):
-                rect.append(str(self.print_symbol))
-            # append new line until the last value of height
-            if x != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        return ""
 
     def __repr__(self):
         """ Produce string representation of the class Rectangle.
@@ -163,4 +155,5 @@ class Rectangle:
         Returns:
             square: new instance of rectangle as square.
         """
+
         return cls(size, size)
--- 0x08-python-more_classes/test.py
+++ 0x08-python-more_classes/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__('rrr').Rectangle
 
-my_square = Rectangle.square(5)
+my_square = Rectangle.square()
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
