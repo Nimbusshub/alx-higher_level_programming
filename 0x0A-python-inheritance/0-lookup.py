793890d5eb8195d5db28b97b0371586d26f8bbad
Nimah <busayonimota1603@gmail.com>
2022-07-04 17:10:19 +0100

    Update the readme file'

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,10 +3,6 @@
 methods of an object"""
 
 
-from click import pass_context
-
-
 def lookup(obj):
-    "returns the available attributes of an object"
-
+    """"returns the available attributes of an object"""
     return dir(obj)
