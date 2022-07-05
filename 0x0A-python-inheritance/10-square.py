a23a6691190e5e35af4939e80574dc38f730d2d7
Nimah <busayonimota1603@gmail.com>
2022-07-05 04:18:25 +0100

    [Fix] Validate the size before initializing

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,5 +9,5 @@ class Square(Rectangle):
     """class Square that inherits from Rectangle"""
 
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
-        self.size = size
