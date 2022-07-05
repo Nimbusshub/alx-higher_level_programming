cc9339186b9ceef1d879a60a7ab51dee58bdcb8c
Nimah <busayonimota1603@gmail.com>
2022-07-05 03:52:29 +0100

    Add the implementation of the public instance method area and defines the behaviour of the subclass

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A subclass of class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry
+    Subclass is initialized with args
+
+    Args:
+        width (int): width of the subclass Rectangle
+        height (int): heigth of the subclass Rectangle
+    """
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        print("[Rectangle] {:d}/{:d}".format(self.__width,
+              self.__height), end="")
+        return ""
