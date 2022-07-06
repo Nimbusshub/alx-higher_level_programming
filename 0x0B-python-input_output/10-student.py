df296a94aec10bf1df942ffffdc33d5d9470f32e
Nimah <busayonimota1603@gmail.com>
2022-07-06 03:19:13 +0100

    Re define the pubic method _json to retrieve file based on attributes if any

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Definition of a class Student"""
+
+
+class Student:
+    """A class Student with public instance attribute
+    first_name, last_name and age
+
+    The class is initiated with:
+
+    Args:
+        first_name (str): student's first name.
+        last_name (str): student's last name.
+        age (int): student's age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrive a dictionary representation of the
+        class instance
+
+        Args:
+            attrs (list): list attributes to retrieve"""
+        if type(attrs) is list:
+            if all(type(item) is str for item in attrs):
+                return {key: getattr(self, key)
+                        for key in attrs if hasattr(self, key)}
+        return self.__dict__
