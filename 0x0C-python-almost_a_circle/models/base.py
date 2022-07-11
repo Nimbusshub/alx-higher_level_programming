910e3874ee7f9026f253516d2928b0860ad0d4a9
Nimah <busayonimota1603@gmail.com>
2022-07-11 15:31:59 +0100

    Write the Json string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,24 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Static method that converts the argument to
+        its JSON representation.
 
+        Args:
+            list_dictionary (dict): list of dictionaries to convert
+        """
         if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """"""
+        file_name = "{}.json".format(cls.__name__)
+        with open(file_name, "w", encoding='utf-8') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                container = cls.to_json_string(list_dict)
+                file.write(container)
