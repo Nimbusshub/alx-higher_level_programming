0c5d9f335b0229d9bc4e2da8faa1dfdd14989f1d
Nimah <busayonimota1603@gmail.com>
2022-09-06 02:12:51 +0100

    Prnts square with X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,15 +2,15 @@
 
 const argvP = Math.trunc(process.argv[2]);
 let i = 0;
-if (!Number(argvP)) console.log("Missing size");
+if (!Number(argvP)) console.log('Missing size');
 else {
   if (argvP >= 1) {
     while (i < Number(argvP)) {
       const cont = [];
       for (let j = 0; j < Number(argvP); j++) {
-        cont.push("X");
+        cont.push('X');
       }
-      console.log(cont.join(""));
+      console.log(cont.join(''));
       i++;
     }
   }
