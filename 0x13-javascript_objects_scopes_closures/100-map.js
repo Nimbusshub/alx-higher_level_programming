15b1dd025788bb0fba61dcf94fb7aa9cba7dd997
Nimah <busayonimota1603@gmail.com>
2022-09-06 18:59:54 +0100

    Computes new array by multiplying elements with index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Array1 = require("./100-data").list;
+const Array1 = require('./100-data').list;
 const Array2 = Array1.map((element, index) => element * index);
 console.log(Array1);
 console.log(Array2);
