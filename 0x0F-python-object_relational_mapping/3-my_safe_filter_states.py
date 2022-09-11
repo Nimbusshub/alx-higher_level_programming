7adb9f9786b573ac3dc51c52c27f107b1ea7fb03
Nimah <busayonimota1603@gmail.com>
2022-09-11 17:14:44 +0100

    Script that takes users input but filters the input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""A script that takes an argument and displays all the values
+in the states table of hbtn_0e_0_usa database
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    condb = MySQLdb.connect(
+        user=sys.argv[1], passwd='PassAlx1!!!', port=3306, db=sys.argv[3])
+    curs = condb.cursor()
+    curs.execute("SELECT * FROM states\
+        WHERE CONVERT(`name` USING latin1)\
+        COLLATE Latin1_General_CS LIKE %s", ("%" + sys.argv[4] + "%",))
+
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+
+    curs.close()
+    condb.close()
