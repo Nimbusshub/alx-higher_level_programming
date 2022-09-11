5796d877876596a7d28ece47da7a70ab0aab468c
Nimah <busayonimota1603@gmail.com>
2022-09-11 15:02:36 +0100

    List all items in states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-condb = MySQLdb.connect(
-    user=sys.argv[1], passwd=sys.argv[2], port=3306, db=sys.argv[3])
-curs = condb.cursor()
-curs.execute("SELECT * FROM states")
-states = curs.fetchall()
-for state in states:
-    print(state)
+    condb = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], port=3306, db=sys.argv[3])
+    curs = condb.cursor()
+    curs.execute("SELECT * FROM states")
+    states = curs.fetchall()
+    for state in states:
+        print(state)
 
-curs.close()
-condb.close()
+    curs.close()
+    condb.close()
