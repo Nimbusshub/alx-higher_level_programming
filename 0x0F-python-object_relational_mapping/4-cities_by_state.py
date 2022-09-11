14b89cc5e9055a161e0b5374bc706c43d9cd100c
Nimah <busayonimota1603@gmail.com>
2022-09-11 19:20:36 +0100

    Lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
         user=sys.argv[1], passwd="PassAlx1!!!", db=sys.argv[3], port=3306)
     curs = condb.cursor()
     curs.execute(
-        "SELECT cities.id,cities.name,states.name FROM cities LEFT JOIN states ON cities.state_id=states.id")
+        "SELECT cities.id,cities.name,states.name FROM cities\
+            LEFT JOIN states ON cities.state_id=states.id")
     cities = curs.fetchall()
 
     for city in cities:
