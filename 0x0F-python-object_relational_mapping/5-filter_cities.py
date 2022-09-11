7f80827bd68f957d80acf318ec9d42dcdb3a9911
Nimah <busayonimota1603@gmail.com>
2022-09-12 00:16:48 +0100

    List all cities of a State in hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""A script that takes in user's input, filters it
+and lists all cities of that state"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    condb = MySQLdb.connect(
+        user=sys.argv[1], passwd="PassAlx1!!!", db=sys.argv[3], port=3306)
+
+    curs = condb.cursor()
+    curs.execute(
+        "SELECT cities.name FROM cities JOIN states ON cities.state_id=states.id\
+            WHERE states.name LIKE %s", ("%" + sys.argv[4] + "%",))
+
+    cities = curs.fetchall()
+    List = []
+    for city in cities:
+        for char in city:
+            List.append(char)
+    print(", ".join(List))
+
+    curs.close()
+    condb.close()
