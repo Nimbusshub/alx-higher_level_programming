94b843deb69c67c54f27f2a7c71507f89aa61982
Nimah <busayonimota1603@gmail.com>
2022-09-12 00:54:36 +0100

    A class State that inherits from Base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """Definition of a class State with module SQLAlchemy"""
+import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
