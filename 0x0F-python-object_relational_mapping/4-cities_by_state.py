4c8ab05291a2df989da92d1d3a7ad814ce8d47f4
Nimah <busayonimota1603@gmail.com>
2022-09-11 19:41:21 +0100

    Lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import sys
 
     condb = MySQLdb.connect(
-        user=sys.argv[1], passwd="PassAlx1!!!", db=sys.argv[3], port=3306)
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = condb.cursor()
     curs.execute(
         "SELECT cities.id,cities.name,states.name FROM cities\
