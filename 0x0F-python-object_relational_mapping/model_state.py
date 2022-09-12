69471e1ce3a29f5e8097dec42b445ebb9c50b764
Nimah <busayonimota1603@gmail.com>
2022-09-12 01:10:46 +0100

    A class State that inherits from Base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
 """Definition of a class State with module SQLAlchemy"""
-import sys
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-""""Base to inherit from"""
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(mymetadata)
 
 
 class State(Base):
-    """"A class with an instance from Base
+    """A class with an instance from Base
     args:
         __table__: initializes the table name as state
         id: (int) unique identifer of the table
         name: (str)
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullabe=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
