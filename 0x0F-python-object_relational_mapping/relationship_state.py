304526e44b8a7172776f81c4b20a428cbfe07180
Nimah <busayonimota1603@gmail.com>
2022-09-12 02:46:23 +0100

    Represent State class and an instance of declarative_base()

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Represents State class and an instance of declarative_base()
 """
-import sys
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -11,14 +11,10 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """Representation of State class
-    args:
-        __tablename___(str): Name of the table
-        id (Integer): unique identifier of table's row
-        name (String): name of states
-        cities (Integer): State-City relationship
+    """
+    Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='states')
+    cities = relationship("City", backref="states")
