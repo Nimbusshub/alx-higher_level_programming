054f2c2da543ac5c71119c74d701457a6b39c9f0
Nimah <busayonimota1603@gmail.com>
2022-09-23 14:33:15 +0100

    Fetch a url specified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 
     with urllib.request.urlopen(url_req) as response:
         res_req = response.read()
-        print('Body response:')
-        print('\t-', 'type:', type(res_req))
-        print('\t-', 'content:', res_req)
-        output = res_req.decode('utf_8')
-        print('\t-', 'utf8 content:', output)
+    print('Body response:')
+    print('\t-', 'type:', type(res_req))
+    print('\t-', 'content:', res_req)
+    output = res_req.decode('utf_8')
+    print('\t-', 'utf8 content:', output)
