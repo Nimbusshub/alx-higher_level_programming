1c08bbf4cfb6ce0b32df3130dbe8f9c3f96a81ab
Nimah <busayonimota1603@gmail.com>
2022-09-23 04:15:08 +0100

    Remove pycodestyle error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""Python script that finds peak in a list of unsorted integers"""
 
-
-from logging.config import listen
+"""Python script that finds
+peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
     """Find the peak of integer"""
+
     if list_of_integers == []:
         return None
     elif len(list_of_integers) == 1:
         return list_of_integers[0]
+
+    li = list_of_integers
+    if len(li) == 2:
+        return max(li)
+
+    length = len(li)
+    mid = length // 2
+    midPoint = li[mid]
+
+    if li[mid + 1] < midPoint and midPoint > li[mid - 1]:
+        return midPoint
+    elif li[mid - 1] > midPoint:
+        return find_peak(li[:mid])
     else:
-        if len(list_of_integers) == 2:
-            return max(list_of_integers)
-
-        length = len(list_of_integers)
-        mid = length // 2
-        midPoint = list_of_integers[mid]
-
-        if list_of_integers[mid + 1] < midPoint and midPoint > list_of_integers[mid - 1]:
-            return midPoint
-        elif list_of_integers[mid - 1] > midPoint:
-            return find_peak(list_of_integers[:mid])
-        else:
-            return find_peak(list_of_integers[mid + 1:])
+        return find_peak(li[mid + 1:])
