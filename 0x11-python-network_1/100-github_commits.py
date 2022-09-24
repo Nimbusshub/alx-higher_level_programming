10cbea9d347212b22978b373afa174099871b515
Nimah <busayonimota1603@gmail.com>
2022-09-24 01:41:45 +0100

    Check for the last 10 commits of a repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
         sys.argv[2], sys.argv[1])
-
-    response = requests.get(url)
-    res_dict = response.json()
-    for i in range(1, 10):
-        print("{}: {}".format(res_dict[i].get('sha'), res_dict[i].get(
-            'commit').get('author').get('name')))
+    try:
+        response = requests.get(url)
+        res_dict = response.json()
+        for i in range(1, 10):
+            print("{}: {}".format(res_dict[i].get('sha'), res_dict[i].get(
+                'commit').get('author').get('name')))
+    except Exception:
+        pass
