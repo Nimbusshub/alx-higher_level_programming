97705b1a5d01848876e24523d7f37c52673d4b59
Nimah <busayonimota1603@gmail.com>
2022-10-18 16:48:10 +0100

    [Fix]: handle error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url).on('response', (response) => {
-  console.log('code: ', response.statusCode);
+request(url, (err, response) => {
+  if (err) console.log(err);
+  else console.log('code:', response.statusCode);
 });
