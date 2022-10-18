f307a8dc8a7babe7acdaf17883a50fe285ab7653
Nimah <busayonimota1603@gmail.com>
2022-10-18 16:37:16 +0100

    [Feats]: Display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Displays the status code of a GET request */
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url).on('response', (response) => {
+  console.log('code: ', response.statusCode);
+});
